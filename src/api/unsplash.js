5c9d6cb867a9d3cba96054dab4129317949cf57e
raynaivanova <rayna.iva89@gmail.com>
2019-07-11 14:18:27 +0200

    Add files via upload

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create( {
+    baseURL: "https://api.unsplash.com",
+    headers: {
+        Authorization: 'Client-ID 43d03eeab764a0629fe42efea9118082c39bb8617d26035a68d4d289c8db44d7'
+    }
+});
